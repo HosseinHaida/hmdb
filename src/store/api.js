fb640b9eed3bde51bca485e6b0b3fc1d9e9d924c
Hossein Heidari <squanderness@gmail.com>
2020-06-21 13:36:06 +0430

    https for omdb

--- src/store/api.js
+++ src/store/api.js
@@ -3,7 +3,7 @@
 
 export default {
   key: "e71ba44dbd36df1e694d29fac0541fa2",
-  omdbBaseUrl: "http://www.omdbapi.com/",
+  omdbBaseUrl: "https://www.omdbapi.com/",
   omdbKey: "1365db7",
   imdbBaseUrl: "https://imdb8.p.rapidapi.com",
   baseUrl: "https://api.themoviedb.org/3",
